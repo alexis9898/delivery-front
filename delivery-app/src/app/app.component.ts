beb5d060f471cafde82eb6d2c049db811e5c9008
alexis9898 <alexle172015@gmail.com>
2023-09-05 21:23:53 +0300

    added console log to app component

--- delivery-app/src/app/app.component.ts
+++ delivery-app/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'delivery-app';
+  constructor() {
+    console.log('shtibel is the kinghggggg');
+  }
 }
